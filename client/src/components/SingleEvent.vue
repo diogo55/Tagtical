a7c018296649a3a0bc34aa9e7b83034815d7d21b
45Yoda <ninjadinosaur.ilikecake@gmail.com>
2019-05-02 21:25:42 +0100

    push de cada evento com tabelas e assim antes que entorne mais cafe no teclado

--- client/src/components/SingleEvent.vue
+++ client/src/components/SingleEvent.vue
@@ -1,12 +1,59 @@
 <template>
-    <div>
-      <h1>Hello</h1>
-    </div>
+  <v-container >
+    <v-layout row wrap>
+      <v-flex xs6>
+        <v-card height=auto width=500 color="blue-grey">
+          <v-card-title class="justify-center">
+          </v-card-title>
+          <v-card-text>
+            <table style="width:100%" border="1" >
+              <tr>
+                <th>{{gamedata.teamA.name}}</th>
+                <th>Velocidade Máxima</th>
+                <th>Velocidade Média</th>
+                <th>Distância</th>
+              </tr>
+              <tr v-for="(player,index) in this.gamedata.teamA.players" :key="index">
+                <td align="center">
+                  {{gamedata.teamA.players[index].name}} </td>
+                <td align="center">
+                  {{gamedata.teamA.players[index].data.vel_max}} </td>
+                <td align="center"> 
+                  {{gamedata.teamA.players[index].data.vel_media}} </td>
+                <td align="center"> 
+                  {{gamedata.teamA.players[index].data.dist}} </td>
+              </tr>
+            </table>
+            <br>
+            <table style="width:100%" border="1" >
+              <tr>
+                <th>{{gamedata.teamB.name}}</th>
+                <th>Velocidade Máxima</th>
+                <th>Velocidade Média</th>
+                <th>Distância</th>
+              </tr>
+              <tr v-for="(player,index) in this.gamedata.teamB.players" :key="index">
+                <td align="center">
+                  {{gamedata.teamB.players[index].name}} </td>
+                <td align="center">
+                  {{gamedata.teamB.players[index].data.vel_max}} </td>
+                <td align="center"> 
+                  {{gamedata.teamB.players[index].data.vel_media}} </td>
+                <td align="center"> 
+                  {{gamedata.teamB.players[index].data.dist}} </td>
+              </tr>
+            </table>
+          </v-card-text>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
+
+export default {
+  name: "SingleEvent",
+  props: ["gamedata"]
+};
 </script>
-
-<style scoped>
-
-</style>
--- client/src/views/Single.vue
+++ client/src/views/Single.vue
@@ -1,6 +1,35 @@
 <template>
-  <div class="about">
-    <h1>Funcionei</h1>
-    <p>Bliss</p>
-  </div>
+  <v-container>
+    <v-layout row wrap>
+      <v-flex>
+        <SingleEvent v-bind:gamedata="gamedata"/>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
+
+<script>
+import SingleEvent from "../components/SingleEvent";
+import axios from 'axios';
+
+export default {
+  name: "Single",
+  components: {
+    SingleEvent
+  },
+  data(){
+    return {
+      gamedata: []
+    }
+  },
+
+  created(){
+    var id = this.$route.params.id
+
+    axios.get(`http://localhost:5000/api/games/${id}`)
+      .then(res => this.gamedata = res.data)
+      .catch(err => console.log(err));
+
+  }
+};
+</script>
