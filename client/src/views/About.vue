9cdd9025d1522db31c0dc37db3c93ba55fe8b2a7
45Yoda <ninjadinosaur.ilikecake@gmail.com>
2019-05-13 15:54:39 +0100

    p5

--- client/src/views/About.vue
+++ client/src/views/About.vue
@@ -6,27 +6,47 @@
 <script>
 import VueP5 from 'vue-p5'
 
+var position = {x:200, y:200},
+  target = {x:400, y:400},
+  step = normalize({x: target.x - position.x, y: target.y - position.y});
+
+function normalize(v)
+{
+   var length = Math.sqrt(v.x * v.x + v.y * v.y);
+   return {x: v.x / length, y: v.y / length};
+}
+
 export default{
   components:{
     "vue-p5": VueP5
   },
   methods:{
     setup(sketch){
-      x = 0;
-      y = 0;
+      VueP5.disableFriendlyErrors = true
       sketch.resizeCanvas(400,400);
     },
+
     draw(sketch){
       sketch.background("darkgreen");
       sketch.fill(0);
-      sketch.ellipse(x, y, 10,10);
+      sketch.ellipse(position.x, position.y, 10,10);
 
-      x = x+random(-1,1);
+      position.x += step.x;
+      position.y += step.y;
+
+      if(position.y > 400){
+          position.y = 0;
+        }
+
+      if(position.x > 400){
+        position.x = 0;
+      }
+      /*x = x+random(-1,1);
       y = y-1;
 
       if(y < 0 ){
         y = height;
-      }
+      }*/
     }
   }
 }
