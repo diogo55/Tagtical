fa8b9631060991d358020c61d5e7633a0438e555
45Yoda <ninjadinosaur.ilikecake@gmail.com>
2019-05-15 13:47:08 +0100

    yo

--- client/src/views/About.vue
+++ client/src/views/About.vue
@@ -1,31 +1,53 @@
 <template>
-  <v-container>
-    <vgl-renderer antialias style="height: 100vh;">
-      <vgl-scene>
-        <vgl-sphere-geometry name="sphere"></vgl-sphere-geometry>
-        <vgl-mesh-standard-material name="std"></vgl-mesh-standard-material>
-        <vgl-mesh geometry="sphere" material="std"></vgl-mesh>
-        <vgl-ambient-light color="#ffeecc"></vgl-ambient-light>
-        <vgl-directional-light position="0 1 1"></vgl-directional-light>
-      </vgl-scene>
-      <vgl-perspective-camera orbit-position="5 1 1;"></vgl-perspective-camera>
-    </vgl-renderer>
-  </v-container>
+    <div id="app">
+      <vgl-renderer antialias style="height: 100vh;">
+        <vgl-scene>
+          <vgl-sphere-geometry name="sphere" radius="25"></vgl-sphere-geometry>
+          <vgl-mesh-standard-material name="std"></vgl-mesh-standard-material>
+          <vgl-mesh geometry="sphere" material="std" :position="`${x} ${y} ${z}`"></vgl-mesh>
+          <vgl-axes-helper size="140"></vgl-axes-helper>
+          <vgl-ambient-light color="#ffeecc"></vgl-ambient-light>
+          <vgl-directional-light position="0 1 1"></vgl-directional-light>
+        </vgl-scene>
+        <vgl-perspective-camera orbit-position="200 1 0.5"></vgl-perspective-camera>
+      </vgl-renderer>
+      <aside class="control-panel">
+        <h3>Position</h3>
+        <label>
+          x
+          <input type="range" v-model="x">
+        </label>
+        <label>
+          y
+          <input type="range" v-model="y">
+        </label>
+        <label>
+          z
+          <input type="range" v-model="z">
+        </label>
+      </aside>
+    </div>
 </template>
 
 
 <script>
-import * as VueGL from 'vue-gl'
 
-var position = {x:200, y:200},
-  target = {x:400, y:400}
+//var position = { x: 200, y: 200 },
+//  target = { x: 400, y: 400 };
 
-export default{
-  components:{
-    "vue-gl": VueGL
-  }
-}
-
+export default {
+  data(){
+      return {
+        x : 50,
+        y : 50,
+        z : 50
+      }
+  },
+};
 </script>
 
 
+ data(){
+    return {
+      events: []
+    }
