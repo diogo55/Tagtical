f17e1bf4848d35b76e456b9f19871fe164a1dc0d
diogo55 <stifler55@Stifler.home>
2019-06-04 23:33:55 +0100

    sidebar

---
+++ client/src/components/Sidebar.vue
@@ -0,0 +1,36 @@
+<template>
+  <ul class="nav nav-pills nav-stacked sidebar">
+      <v-card max-width=150>
+      <p align= "center"><b> {{this.gamedata.teamA.name}}</b><button v-on:click="seen1 = !seen1"><b> +</b></button></p>
+        <tr v-for="(player,index) in this.gamedata.teamA.players" :key="index">
+            <td v-if="seen1">
+              {{gamedata.teamA.players[index].name}} </td>
+        </tr>
+        </v-card>
+
+        <v-card max-width=150>
+            <ul/>
+      <p align= "center"><b> {{this.gamedata.teamB.name}}</b><button v-on:click="seen2 = !seen2"><b> +</b></button></p>
+        <tr v-for="(player,index) in this.gamedata.teamB.players" :key="index">
+            <td v-if="seen2">
+              {{gamedata.teamB.players[index].name}} </td>
+        </tr>
+    </v-card>
+  </ul>
+</template>
+
+
+<script>
+export default {
+    name: "Sidebar",
+    props: ["gamedata"],
+    data() {
+        return{
+            seen1: false,
+            seen2: false
+    }
+}
+}
+</script>
+
+
