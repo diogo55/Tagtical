66d230947738f40ce44c7e0489de6e22158d563b
45Yoda <ninjadinosaur.ilikecake@gmail.com>
2019-06-07 22:42:15 +0100

    sidebar de equipas feito

--- client/src/views/Single.vue
+++ client/src/views/Single.vue
@@ -26,56 +26,74 @@
         </v-list>
 
         <v-list>
-          <v-list-tile>
-            <v-list-tile-action>
-              <v-icon>group</v-icon>
-            </v-list-tile-action>
-            <v-list-tile-content>
-              <v-list-tile-title>{{this.gamedata.teamA.name}}</v-list-tile-title>
-              <v-list>
-                <v-list-tile
-                  v-for="(player,index) in this.gamedata.teamA.players"
-                  :key="index"
-                  router
-                  :to="{name:'player',params:{id:gamedata._id,team:'A',n:index}}"
-                >
-                  <v-list-tile-action>
-                    <v-icon>group</v-icon>
-                  </v-list-tile-action>
-                  <v-list-tile-content>
-                    <v-list-tile-title>{{gamedata.teamA.players[index].name}}</v-list-tile-title>
-                  </v-list-tile-content>
+          <v-list-group prepend-icon="account_circle" value="true">
+            <template v-slot:activator>
+              <v-list-tile>
+                <v-list-tile-title>{{gamedata.teamA.name}}</v-list-tile-title>
+              </v-list-tile>
+            </template>
+            <v-list-tile :to="{ name: 'team', params: {id: gamedata._id,team: 'A'} }">
+              <v-list-tile-action>
+                <v-icon>event</v-icon>
+              </v-list-tile-action>
+              <v-list-tile-title>Geral</v-list-tile-title>
+            </v-list-tile>
+            <v-list-group prepend-icon="group" no-action sub-group value="true">
+              <template v-slot:activator>
+                <v-list-tile>
+                  <v-list-tile-title>Jogadores</v-list-tile-title>
                 </v-list-tile>
-              </v-list>
-            </v-list-tile-content>
-          </v-list-tile>
-          <v-list-tile>
-            <v-list-tile-action>
-              <v-icon>group</v-icon>
-            </v-list-tile-action>
-            <v-list-tile-content>
-              <v-list-tile-title>{{this.gamedata.teamB.name}}</v-list-tile-title>
-              <v-list>
-                <v-list-tile
-                  v-for="(player,index) in this.gamedata.teamB.players"
-                  :key="index"
-                  router
-                  :to="{name:'player',params:{id:gamedata._id,team:'B',n:index}}"
-                >
-                  <v-list-title-action>
-                    <v-icon>group</v-icon>
-                  </v-list-title-action>
-                  <v-list-tile-content>
-                    <v-list-tile-title>{{gamedata.teamB.players[index].name}}</v-list-tile-title>
-                  </v-list-tile-content>
+              </template>
+
+              <v-list-tile
+                v-for="(player, index) in gamedata.teamA.players"
+                :key="index"
+                router
+                :to="{name:'player',params:{id:gamedata._id,team:'A',n:index}}"
+              >
+                <v-list-tile-action>
+                  <v-icon>person</v-icon>
+                </v-list-tile-action>
+                <v-list-tile-title v-text="gamedata.teamA.players[index].name"></v-list-tile-title>
+              </v-list-tile>
+            </v-list-group>
+          </v-list-group>
+          <v-list-group prepend-icon="account_circle" value="true">
+            <template v-slot:activator>
+              <v-list-tile>
+                <v-list-tile-title>{{gamedata.teamB.name}}</v-list-tile-title>
+              </v-list-tile>
+            </template>
+            <v-list-tile :to="{ name: 'team', params: {id: gamedata._id,team: 'B'} }">
+              <v-list-tile-action>
+                <v-icon>event</v-icon>
+              </v-list-tile-action>
+              <v-list-tile-title>Geral</v-list-tile-title>
+            </v-list-tile>
+            <v-list-group prepend-icon="group" no-action sub-group value="true">
+              <template v-slot:activator>
+                <v-list-tile>
+                  <v-list-tile-title>Jogadores</v-list-tile-title>
                 </v-list-tile>
-              </v-list>
-            </v-list-tile-content>
-          </v-list-tile>
+              </template>
+
+              <v-list-tile
+                v-for="(player, index) in gamedata.teamB.players"
+                :key="index"
+                router
+                :to="{name:'player',params:{id:gamedata._id,team:'B',n:index}}"
+              >
+                <v-list-tile-action>
+                  <v-icon>person</v-icon>
+                </v-list-tile-action>
+                <v-list-tile-title v-text="gamedata.teamB.players[index].name"></v-list-tile-title>
+              </v-list-tile>
+            </v-list-group>
+          </v-list-group>
         </v-list>
       </v-navigation-drawer>
     </div>
-        <router-view :key="$route.fullPath"></router-view>
+    <router-view :key="$route.fullPath"></router-view>
   </div>
 </template>
 
@@ -132,3 +150,4 @@ export default {
   }
 };
 </script>
+
