26c4f0d0926e90c7e84dcc199e739250927bd555
45Yoda <ninjadinosaur.ilikecake@gmail.com>
2019-06-07 16:54:57 +0100

    added event sidebar

--- client/src/views/Single.vue
+++ client/src/views/Single.vue
@@ -1,8 +1,18 @@
 <template>
   <div>
+    <div>
+      <v-btn icon @click.stop="drawer = !drawer">
+        <v-icon>event</v-icon>
+      </v-btn>
+      <v-navigation-drawer v-model="drawer" mobile-break-point="10240" app class="grey">
+        <v-list>
+          <Events v-bind:events="events" v-on:go-game="goGame"/>
+        </v-list>
+      </v-navigation-drawer>
+    </div>
     <Sidebar v-bind:gamedata="gamedata"/>
-   <router-view :key="$route.fullPath"></router-view>
-   </div>
+    <router-view :key="$route.fullPath"></router-view>
+  </div>
 </template>
 
 
@@ -11,17 +21,21 @@ import SingleEvent from "../components/SingleEvent";
 import HeatMap from "../components/HeatMap";
 import axios from "axios";
 import Sidebar from "../components/Sidebar";
+import Events from "../components/Events";
 
 export default {
   name: "Single",
   components: {
     SingleEvent,
     HeatMap,
-    Sidebar
+    Sidebar,
+    Events
   },
   data() {
     return {
+      drawer: false,
       gamedata: [],
+      events: [],
       watch: {
         gamedata() {
           return this.gamedata;
@@ -33,10 +47,18 @@ export default {
   methods: {
     viewGame(id) {
       window.open("http://localhost:5000/files/" + id + "/sh.html");
+    },
+
+    goGame(id) {
+      this.$router.push("/events/" + id);
     }
   },
   created() {
     var id = this.$route.params.id;
+    axios
+      .get("http://localhost:5000/api/games")
+      .then(res => (this.events = res.data))
+      .catch(err => console.log(err));
 
     axios
       .get(`http://localhost:5000/api/games/${id}`)
