d33f06e4816a30b56b3916cbecbb4b4c0ca609c0
45Yoda <ninjadinosaur.ilikecake@gmail.com>
2019-06-25 17:42:48 +0100

    remade about

--- client/src/views/About.vue
+++ client/src/views/About.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="text">
     <p>
-      Technology is being used more and more in the various activities of everyday life.
-      Sports aren't a exception to the rule and everyday there are more cases of technology incorporated in sports,
-      such as: the line goal used in various sports and more recently the introduction of VAR on football.
-
-      This evolution doens't only happen just on the game but on the training too, where the gathering and analysis 
-      of the data allow the trainer to get a better comprehension of the player's performance and maximize his potential.
-
-      It's in this field that we want to make a difference. We want gather the data of the players in a single game
-      and show it in a way that a trainer can improve his knowledge about his team players and performance.
+      These days technology can be found in everything in our life, from medicine to education. 
+      Sports aren't an exception and everyday there are more cases of technology incorporated in sports,
+      such as: goal line technology used in various sports and more recently the introduction of VAR in football.
+      This technology isn't only directed at the games there has also been an evolution in the way that coaches train their players. The 
+      gathering and analysis of game data can be very useful for a coach to get a better ideia of a player's capabilities and help maximize
+      his potential.
+      It's here that Tagtical Hockey tries to make a difference. We want to gather the data of the players in a single game and show it 
+      in a way that a coach can improve his knowledge about his team players and also improve performance.
 
       This type of technology is already being used in some outdoors sports but we challenge ourselves to bring this to indoor sports too.
-</p>
+    </p>
   </div>
 </template>
 
